3510f89d952343b555c3a441ff0066e08b7d11db
Bluesbrick <142378915+D-Kai-Nie@users.noreply.github.com>
2026-05-28 09:35:01 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -56,7 +56,7 @@ export const targetConfig = [{
 export const backendConfig = [
     {
         label: 'Kai-自建后端',
-        value: 'https://kofogrtditqe.ap-northeast-1.clawcloudrun.com'
+        value: 'https://bluesbrick-my-subconverter.hf.space'
     }, {
         label: 'CM提供-负载均衡后端',
         value: 'https://subapi.cmliussss.net/sub?'
